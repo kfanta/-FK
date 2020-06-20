772b61905114ec54b260a0a39ce9dbb4d1a5ef38
Richlove Nkansah <richlovenkansah@gmail.com>
2020-06-20 12:11:31 -0400

    Updated quix.xml,java

--- MyMultiscreenApp2/Login/app/src/main/java/com/example/login/ui/login/Question.java
+++ MyMultiscreenApp2/Login/app/src/main/java/com/example/login/ui/login/Question.java
@@ -1,4 +1,59 @@
 package com.example.login.ui.login;
 
 public class Question {
+    private String title;
+    private String response1;
+    private String response2;
+    private String response3;
+    private String response4;
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getResponse1() {
+        return response1;
+    }
+
+    public void setResponse1(String response1) {
+        this.response1 = response1;
+    }
+
+    public String getResponse2() {
+        return response2;
+    }
+
+    public void setResponse2(String response2) {
+        this.response2 = response2;
+    }
+
+    public String getResponse3() {
+        return response3;
+    }
+
+    public void setResponse3(String response3) {
+        this.response3 = response3;
+    }
+
+    public String getResponse4() {
+        return response4;
+    }
+
+    public void setResponse4(String response4) {
+        this.response4 = response4;
+    }
+
+    public Question(String title, String response1, String response2, String response3, String response4) {
+        this.title = title;
+        this.response1 = response1;
+        this.response2 = response2;
+        this.response3 = response3;
+        this.response4 = response4;
+
+    }
 }
--- MyMultiscreenApp2/Login/app/src/main/java/com/example/login/ui/login/QuizActivity.java
+++ MyMultiscreenApp2/Login/app/src/main/java/com/example/login/ui/login/QuizActivity.java
@@ -3,14 +3,25 @@ package com.example.login.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.login.R;
 
 public class QuizActivity extends AppCompatActivity {
-
+    TextView questions;
+    TextView answerchoice1;
+    TextView answerchoice2;
+    TextView answerchoice3;
+    TextView answerchoice4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        questions = findViewById(R.id.questions);
+        answerchoice1 = findViewById(R.id.answerchoice1);
+        answerchoice2 = findViewById(R.id.answerchoice2);
+        answerchoice3 = findViewById(R.id.answerchoice3);
+        answerchoice4 = findViewById(R.id.answerchoice4);
     }
 }
