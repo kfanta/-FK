c9e42fadfaf7f63c59b5819638b5d5a6aaa3c88d
Richlove Nkansah <richlovenkansah@gmail.com>
2020-06-22 16:17:46 -0400

    Updated quizactivity.java

--- MyMultiscreenApp2/Login/app/src/main/java/com/example/login/ui/login/QuizActivity.java
+++ MyMultiscreenApp2/Login/app/src/main/java/com/example/login/ui/login/QuizActivity.java
@@ -3,22 +3,24 @@ package com.example.login.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.login.R;
 
 public class QuizActivity extends AppCompatActivity {
-    TextView questions;
-    TextView answerchoice1;
-    TextView answerchoice2;
-    TextView answerchoice3;
-    TextView answerchoice4;
+    TextView Questions;
+    Button answerchoice1;
+    Button answerchoice2;
+    Button answerchoice3;
+    Button answerchoice4;
+
     @Override //personality quiz
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        questions = findViewById(R.id.questions);
+        Questions = findViewById(R.id.Questions);
         answerchoice1 = findViewById(R.id.answerchoice1);
         answerchoice2 = findViewById(R.id.answerchoice2);
         answerchoice3 = findViewById(R.id.answerchoice3);
